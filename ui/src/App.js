f0ddccf15b4c313eac70709e1b9d5c83db064a2e
edd-reyes <pedroeddie.reyes@gmail.com>
2022-09-15 18:02:13 +0800

    Added Home page and other components

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,24 @@
-import './App.css';
-import Register from './components/Register';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import "./App.css";
+import Register from "./components/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import FAQs from "./components/FAQs";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <ScrollToTop/>
+      <Navbar/>
         <Routes>
-          <Route path='/register' element={<Register />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/FAQs" element={<FAQs />}></Route>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
